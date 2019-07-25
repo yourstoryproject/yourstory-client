31c037d406f65342e633050979d1fc4494a1441d
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-07-24 17:36:34 -0700

    Add firebase config object

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,16 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID,
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
