2b5366c27917cc27fd3f2d7743723ec4bcb205d4
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-11 18:13:05 -0700

    YSP-0 Add firebase context and login configs

---
+++ src/js/context/firebase-auth-context.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import firebase from 'firebase/app';
+
+const defaultFirebaseContext = {
+	authStatusReported: false,
+	email: '',
+	isUserSignedIn: false,
+	uid: '',
+};
+
+export const FirebaseAuthContext = React.createContext(defaultFirebaseContext);
+
+export default class FirebaseAuthProvider extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = defaultFirebaseContext;
+	}
+
+	componentDidMount() {
+		firebase.auth().onAuthStateChanged(user => {
+			if (user) {
+				this.setState({
+					authStatusReported: true,
+					email: user.email,
+					isUserSignedIn: !!user,
+					uid: user.uid,
+				}),
+			}
+
+		}
+		);
+	}
+
+	componentWillUnmount() {
+		
+	}
+
+	render() {
+		const {
+			props: { children },
+			state: { authStatusReported, isUserSignedIn },
+		} = this;
+
+		return (
+			<FirebaseAuthContext.Provider
+				value={this.state}
+			>
+				{authStatusReported && children}
+			</FirebaseAuthContext.Provider>
+		);
+	}
+}
