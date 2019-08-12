d8d7e97e971c102227375bad0f7b148238c2a106
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-12 07:46:31 -0700

    YSP-0 Init firebase, add eslint rules, update formatting

--- src/js/App.js
+++ src/js/App.js
@@ -1,55 +1,47 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
-import Signin from "./pages/Signin";
-import { Signout } from "./lib/firebase";
-import styles from "styled-components";
-import FirebaseAuthProvider from "./context/firebase-auth-context";
-import firebase from "firebase/app";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import { Signout } from './lib/firebase';
+import FirebaseAuthProvider from './context/firebase-auth-context';
+import firebase from 'firebase/app';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      theme,
-      user: {
-        email: "",
-        uid: ""
-      }
+    state = {
+        user: {
+            email: '',
+            uid: ''
+        }
     };
-  }
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({
-          user: {
-            email: user.email,
-            uid: user.uid
-          }
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.setState({
+                    user: {
+                        email: user.email,
+                        uid: user.uid
+                    }
+                });
+            } else {
+                console.log('No user signed in');
+            }
         });
-      } else {
-        console.log("No user signed in");
-      }
-    });
-  }
+    }
 
-  render() {
-    return (
-      <FirebaseAuthProvider>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/user/signin" component={Signin} />
-            <Route path="/user/signout" component={Signout} />
-            {/* <Route path="/user/:userId" component={User} /> */}
-            {/* <Route path="*" component={NotFound} /> */}
-          </Switch>
-        </Router>
-      </FirebaseAuthProvider>
-    );
-  }
+    render() {
+        return (
+            <FirebaseAuthProvider>
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/user/signin" component={Signin} />
+                        <Route path="/user/signout" component={Signout} />
+                        {/* <Route path="/user/:userId" component={User} /> */}
+                        {/* <Route path="*" component={NotFound} /> */}
+                    </Switch>
+                </Router>
+            </FirebaseAuthProvider>
+        );
+    }
 }
-
-App.contextType = ThemeContext;
