6ee261eea901d16559eaec735ff044012e2e6f7c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-13 13:15:22 -0700

    YSP-0 Remove auth check on entire application and some formatting

--- src/js/context/firebase-auth-context.js
+++ src/js/context/firebase-auth-context.js
@@ -1,47 +1,48 @@
-import React from 'react';
-import firebase from 'firebase/app';
+import React from "react";
+import firebase from "firebase/app";
 
 const defaultFirebaseContext = {
-    authStatusReported: false,
-    email: '',
-    isUserSignedIn: false,
-    uid: ''
+  authStatusReported: false,
+  email: "",
+  isUserSignedIn: false,
+  uid: ""
 };
 
 export const FirebaseAuthContext = React.createContext(defaultFirebaseContext);
 
 export default class FirebaseAuthProvider extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = defaultFirebaseContext;
-    }
+    this.state = defaultFirebaseContext;
+  }
 
-    componentDidMount() {
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                this.setState({
-                    authStatusReported: true,
-                    email: user.email,
-                    isUserSignedIn: !!user,
-                    uid: user.uid
-                });
-            }
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authStatusReported: true,
+          email: user.email,
+          isUserSignedIn: !!user,
+          uid: user.uid
         });
-    }
+      }
+    });
+  }
 
-    componentWillUnmount() {}
+  componentWillUnmount() {}
 
-    render() {
-        const {
-            props: { children },
-            state: { authStatusReported, isUserSignedIn }
-        } = this;
+  render() {
+    const {
+      props: { children },
+      state: { authStatusReported, isUserSignedIn }
+    } = this;
 
-        return (
-            <FirebaseAuthContext.Provider value={this.state}>
-                {authStatusReported && children}
-            </FirebaseAuthContext.Provider>
-        );
-    }
+    return (
+      <FirebaseAuthContext.Provider value={this.state}>
+        {/* {authStatusReported && children} */}
+        {children}
+      </FirebaseAuthContext.Provider>
+    );
+  }
 }
