2d7b176f60cb13acc719d39c88e4485afc16878c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-13 13:17:36 -0700

    YSP-0 Add prettier configs, rerunformatter

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
-import "../css/main.scss";
-import App from "./App";
-import ReactDOM from "react-dom";
-import React from "react";
+import '../css/main.scss';
+import App from './App';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
-ReactDOM.render(<App />, document.getElementById("yourStoryProjectApp"));
+ReactDOM.render(<App />, document.getElementById('yourStoryProjectApp'));
--- src/js/pages/Home.js
+++ src/js/pages/Home.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 export default class Home extends React.Component {
-  render() {
-    return <div className="home-container">Hello</div>;
-  }
+	render() {
+		return <div className="home-container">Hello</div>;
+	}
 }
