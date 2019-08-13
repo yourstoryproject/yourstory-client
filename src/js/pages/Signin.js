6154f5acc138e48279763181153c0abc33c7d60c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-13 13:15:40 -0700

    YSP-0 SF

--- src/js/pages/Signin.js
+++ src/js/pages/Signin.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 export default class Signin extends React.Component {
-    render() {
-        return <div className="signin-container"></div>;
-    }
+  render() {
+    return <div className="signin-container"></div>;
+  }
 }
