108b09bd68b3bc2b605607a89cdb18f7ec597fe0
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-13 13:15:40 -0700

    YSP-0 SF

--- src/js/lib/auth.js
+++ src/js/lib/auth.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export const uiConfig = {
-    //TODO: Add routes for redirects
-    signInSuccessUrl: '/home',
-    signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-    ],
-    // tosUrl and privacyPolicyUrl accept either url string or a callback
-    // function.
-    // Terms of service url/callback.
-    tosUrl: '<your-tos-url>',
-    // Privacy policy url/callback.
-    privacyPolicyUrl: function() {
-        window.location.assign('<your-privacy-policy-url>');
-    }
+  //TODO: Add routes for redirects
+  signInSuccessUrl: "/home",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign("<your-privacy-policy-url>");
+  }
 };
