e88acb21a09135eb9627835c73763aa291473ca1
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-08-29 19:42:22 -0700

    YSP-7 Add api client for stories

--- src/js/lib/api-client/index.js
+++ src/js/lib/api-client/index.js
@@ -1,9 +1,12 @@
 import * as identities from './identities';
+import * as stories from './stories';
 
 export const identitiesRef = identities;
+export const storiesRef = stories;
 
 const api = {
 	identitiesRef,
+	storiesRef,
 };
 
 export default api;
