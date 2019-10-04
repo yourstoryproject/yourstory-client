033243107acb72658ad315897f1432fc9a74e575
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-10-03 22:11:49 -0700

    YSP-15 Add file loader for image processing

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,27 @@
 module.exports = {
 	collectCoverage: true,
-	collectCoverageFrom: [
-		'<rootDir>/src/js/**/components/**/*.js',
-	],
+	collectCoverageFrom: ['<rootDir>/src/js/**/components/**/*.js'],
 	coverageThreshold: {
 		global: {
 			branches: 55,
 			functions: 55,
 			lines: 65,
-			statements: 65
-		}
+			statements: 65,
+		},
 	},
 	globals: {
-    yourStoryConstants: {},
-  },
+		yourStoryConstants: {},
+	},
 	setupFiles: ['<rootDir>/enzyme.config.js'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+	setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 	snapshotSerializers: ['enzyme-to-json/serializer'],
 	timers: 'fake',
 	transform: {
-		'^.+\\.js?$': 'babel-jest'
-	}
-}
+		'^.+\\.js?$': 'babel-jest',
+	},
+	moduleNameMapper: {
+		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+			'<rootDir>/assetsTransformer.js',
+		'\\.(css|less)$': '<rootDir>/assetsTransformer.js',
+	},
+};
--- webpack.common.js
+++ webpack.common.js
@@ -1,75 +1,83 @@
-const path = require("path");
+const path = require('path');
 
-const CleanWebpackPlugin = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const autoprefixer = require("autoprefixer");
-const Dotenv = require("dotenv-webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+const Dotenv = require('dotenv-webpack');
 
-const PUBLIC_PATH = path.resolve(__dirname, "/");
+const PUBLIC_PATH = path.resolve(__dirname, '/');
 
-function resolveModule(name = "") {
-  return path.resolve(__dirname, "src", "js", name);
+function resolveModule(name = '') {
+	return path.resolve(__dirname, 'src', 'js', name);
 }
 
 const include = [resolveModule()];
 
 const config = {
-  entry: resolveModule("main.js"),
-  module: {
-    rules: [
-      {
-        exclude: /(node_modules)/,
-        test: /\.js$/,
-        include,
-        use: {
-          loader: "babel-loader",
-          options: { presets: ["@babel/preset-env"] }
-        }
-      },
-      {
-        include: path.resolve(__dirname, "src", "css"),
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { importLoaders: 2 }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              ident: "postcss",
-              plugins: () => [require("autoprefixer")()]
-            }
-          },
-          {
-            loader: "sass-loader"
-          }
-        ]
-      }
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
-    publicPath: PUBLIC_PATH
-  },
-  plugins: [
-    new Dotenv(),
-    new MiniCssExtractPlugin({
-      filename: "main.css"
-    }),
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      title: "Your Story Project"
-    })
-  ],
-  target: "web"
+	entry: resolveModule('main.js'),
+	module: {
+		rules: [
+			{
+				exclude: /(node_modules)/,
+				test: /\.js$/,
+				include,
+				use: {
+					loader: 'babel-loader',
+					options: { presets: ['@babel/preset-env'] },
+				},
+			},
+			{
+				include: path.resolve(__dirname, 'src', 'css'),
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: { importLoaders: 2 },
+					},
+					{
+						loader: 'postcss-loader',
+						options: {
+							ident: 'postcss',
+							plugins: () => [require('autoprefixer')()],
+						},
+					},
+					{
+						loader: 'sass-loader',
+					},
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+		],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.bundle.js',
+		publicPath: PUBLIC_PATH,
+	},
+	plugins: [
+		new Dotenv(),
+		new MiniCssExtractPlugin({
+			filename: 'main.css',
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			title: 'Your Story Project',
+		}),
+	],
+	target: 'web',
 };
 
 module.exports = {
-  config,
-  include,
-  publicPath: PUBLIC_PATH
+	config,
+	include,
+	publicPath: PUBLIC_PATH,
 };
