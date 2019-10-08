e060c263e110f86305ad233595d4059af053500c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-10-07 19:57:20 -0700

    YSP-0 Convert SignIn to functional component

--- src/js/pages/Signin.tsx
+++ src/js/pages/Signin.tsx
@@ -3,15 +3,13 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/app';
 import { uiConfig } from '../lib/auth';
 
-export default class Signin extends React.Component {
-	render() {
-		return (
-			<div className="signin-container">
-				<StyledFirebaseAuth
-					uiConfig={uiConfig}
-					firebaseAuth={firebase.auth()}
-				/>
-			</div>
-		);
-	}
-}
+const SignIn = () => (
+	<div className="signin-container">
+		<StyledFirebaseAuth
+			uiConfig={uiConfig}
+			firebaseAuth={firebase.auth()}
+		/>
+	</div>
+);
+
+export default SignIn;
